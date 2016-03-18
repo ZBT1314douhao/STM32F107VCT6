0bb119f37287cfc2385f58b188211cf84c946298
zbt1314douhao <zbt1314douhao@live.com>
2016-03-18 15:52:27 +0800

    ledV0.1
    Signed-off-by: zbt1314douhao <zbt1314douhao@live.com>

---
+++ 1、ledV0.1/user_lib/delay.c
@@ -0,0 +1,45 @@
+/**
+  *
+  * @file    delay.c 
+  * @author  Z B T
+  * @version V0.1
+  * @date    2016-
+  * @brief   
+  *
+  */  
+
+#include "delay.h"
+
+/**
+ *  @brief  ¥÷¬‘—” ±n us
+ *  @param  None
+ *  @return None
+ */
+void delay_n_us(uint32_t n_us)
+{
+  uint8_t i = 0;
+
+  while (n_us--)
+  {
+    i = 10;
+    while (i--);
+  }
+}
+
+/**
+ *  @brief  ¥÷¬‘—” ±n ms
+ *  @param  None
+ *  @return None
+ */
+void delay_n_ms(uint32_t n_ms)
+{
+  uint16_t i = 0;
+
+  while (n_ms--)
+  {
+    i = 12000;
+    while (i--);
+  }
+}
+
+
