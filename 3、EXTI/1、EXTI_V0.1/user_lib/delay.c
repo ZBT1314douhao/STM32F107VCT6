dcb362485af3b491d8cd321ec129f32ef72ab1da
zbt1314douhao <zbt1314douhao@live.com>
2016-03-21 16:54:23 +0800

    外部中断 PA0--KEY1 PC13--KEY2
    Signed-off-by: zbt1314douhao <zbt1314douhao@live.com>

---
+++ 3、EXTI/1、EXTI_V0.1/user_lib/delay.c
@@ -0,0 +1,45 @@
+/**
+  *
+  * @file    delay.c 
+  * @author  Z B T
+  * @version V0.1
+  * @date    2016-
+  * @brief   
+  *
+  */  
+
+#include "delay.h"
+
+/**
+ *  @brief   延时n_us
+ *  @param  [in]n_us	粗略延时
+ *  @return None
+ */
+void delay_n_us(uint32_t n_us)
+{
+    uint8_t i = 0;
+
+    while (n_us--)
+    {
+        i = 10;
+        while (i--);
+    }
+}
+
+/**
+ *  @brief  延时n_ms
+ *  @param  [in] n_ms 粗略延时
+ *  @return None
+ */
+void delay_n_ms(uint32_t n_ms)
+{
+    uint16_t i = 0;
+
+    while (n_ms--)
+    {
+        i = 12000;
+        while (i--);
+    }
+}
+
+
