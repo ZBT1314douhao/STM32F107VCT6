761fea86d1bdc5f7f7ca10c0a00548f786c0a492
zbt1314douhao <zbt1314douhao@live.com>
2016-04-30 09:41:45 +0800

    Update: 新增ADC例程 中断法 查询法 DMA方式
    Signed-off-by: zbt1314douhao <zbt1314douhao@live.com>

---
+++ 6. ADC/1. LCD显示AD的转换值(DMA)--基于STemwin/user_lib/adc_dup.c
@@ -0,0 +1,53 @@
+/**
+  * @file       adc_dup.c 
+  * @author     Z B T
+  * @version    V0.1
+  * @date       2016- 04
+  * @brief      AD转换处理程序 DMA方式
+  *
+  */  
+
+#include "adc_dup.h"
+#include "adc.h"
+#include "delay.h"
+
+#define ADC_BUFFER_SIZE     16
+
+static __IO uint16_t adc_buff[ADC_BUFFER_SIZE];
+
+ /**
+  *  @brief  启动ADC
+  *  @param  None  		
+  *  @return None
+  */
+void ADC_start(void)
+{
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_buff, ADC_BUFFER_SIZE);
+}
+
+ /**
+  *  @brief  将采样值转换为电压值 mV
+  *  @param  None	
+  *  @return None
+  */
+uint16_t ADC_get_value(void)
+{
+    uint8_t  i = 0;
+    uint16_t value = 0;
+    uint32_t adc_value = 0;
+    
+    for (i = 0; i < ADC_BUFFER_SIZE; i++)
+    {
+        adc_value += adc_buff[i];
+    }
+    
+    adc_value >>= 4;        /**< 此处等价于adc_value / ADC_BUFFER_SIZE */
+    value = (uint16_t)(3300 * adc_value / 4096);
+    
+    return value;
+}
+
+
+
+
+
