b04a5cb975c659feb787ed49ee6f5e2f96bfe4ea
zbt1314douhao <zbt1314douhao@live.com>
2016-05-01 12:53:41 +0800

    Update ball
    Signed-off-by: zbt1314douhao <zbt1314douhao@live.com>

---
+++ 5、LCD/7、STemwin小球碰撞V0.1/user_lib/gui_clock.h
@@ -0,0 +1,26 @@
+/**
+  *
+  * @file       gui_clock.h 
+  * @author     Z B T
+  * @version    V0.1
+  * @date       2016 - 04
+  * @brief      基于STemwin的时钟
+  *
+  */  
+
+#ifndef __GUI_CLOCK_H
+#define __GUI_CLOCK_H
+
+#include "stm32f1xx_hal.h"
+
+void draw_clock(void);
+void display_clock(void);
+
+struct CIRCLE_INIT
+{
+    uint16_t center_x;
+    uint16_t center_y;
+    uint8_t  raduis;
+};
+
+#endif
